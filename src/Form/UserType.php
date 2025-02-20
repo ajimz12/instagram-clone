4d85417fc56d4fa77c39e2df4f0008255b8ee559
Fer-r <fer_r_a@outlook.es>
2025-02-20 18:50:25 +0100

    cruds added

---
+++ src/Form/UserType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username')
+            ->add('roles')
+            ->add('password')
+            ->add('follow', EntityType::class, [
+                'class' => User::class,
+'choice_label' => 'id',
+'multiple' => true,
+            ])
+            ->add('users', EntityType::class, [
+                'class' => User::class,
+'choice_label' => 'id',
+'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
